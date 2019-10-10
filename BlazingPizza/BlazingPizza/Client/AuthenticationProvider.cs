36d83f28f320b1e3cc0e06a0fdf90546d5943539
Michael Marango <mike.marango@gmail.com>
2019-10-10 10:51:14 +0300

    Login using Google, Twitter, Microsoft etc

--- BlazingPizza/BlazingPizza/Client/AuthenticationProvider.cs
+++ BlazingPizza/BlazingPizza/Client/AuthenticationProvider.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
+﻿using BlazingPizza.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -9,11 +12,19 @@ namespace BlazingPizza.Client
 {
     public class AuthenticationProvider : AuthenticationStateProvider
     {
-        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        private readonly HttpClient http;
+
+        public AuthenticationProvider(HttpClient http)
         {
-            var claim = new Claim(ClaimTypes.Name, "Fake user");
-            var identity = new ClaimsIdentity(new[] { claim }, "serverauth");
-            return Task.FromResult(new AuthenticationState(new ClaimsPrincipal(identity)));
+            this.http = http;
+        }
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var userInfo = await http.GetJsonAsync<UserInfo>("user");
+            var identity = userInfo.IsAuthenticated
+                ? new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, userInfo.Name) }, "serverauth")
+                : new ClaimsIdentity();
+            return new AuthenticationState(new ClaimsPrincipal(identity));
         }
     }
 }
