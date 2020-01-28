b2071f0300607be2bb75a800a171eceb64659150
Michael Marango <mike.marango@gmail.com>
2020-01-28 09:33:20 +0300

    Social media signin

--- BlazingPizza/BlazingPizza/Client/ServerAuthenticationStateProvider.cs
+++ BlazingPizza/BlazingPizza/Client/ServerAuthenticationStateProvider.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
+﻿using BlazingPizza.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -9,11 +12,27 @@ namespace BlazingPizza.Client
 {
     public class ServerAuthenticationStateProvider : AuthenticationStateProvider
     {
+        public ServerAuthenticationStateProvider(HttpClient httpClient)
+        {
+            HttpClient = httpClient;
+        }
+
+        public HttpClient HttpClient { get; }
+
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            var claim = new Claim(ClaimTypes.Name, "Fake Account");
-            var identity = new ClaimsIdentity(new[] { claim }, "serverauth");
-            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(identity)));
+            var userInfo = await HttpClient.GetJsonAsync<UserInfo>("user");
+            var identity = userInfo.IsAuthenticated
+                ? new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, userInfo.Name) }, "serverauth")
+                : new ClaimsIdentity();
+            return new AuthenticationState(new ClaimsPrincipal(identity));
+
+
+
+
+            //var claim = new Claim(ClaimTypes.Name, "Fake Account");
+            //var identity = new ClaimsIdentity(new[] { claim }, "serverauth");
+            //return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(identity)));
         }
     }
 }
