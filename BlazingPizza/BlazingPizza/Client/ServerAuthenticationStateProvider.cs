3c491b4cdb14cec09088cdc07cfe9eca88a19d73
Michael Marango <mike.marango@gmail.com>
2020-01-28 09:17:50 +0300

    Track authentication state

---
+++ BlazingPizza/BlazingPizza/Client/ServerAuthenticationStateProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Client
+{
+    public class ServerAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var claim = new Claim(ClaimTypes.Name, "Fake Account");
+            var identity = new ClaimsIdentity(new[] { claim }, "serverauth");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(identity)));
+        }
+    }
+}
